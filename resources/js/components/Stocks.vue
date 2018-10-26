b2c7711728f0e35f2c2d309e5ace03aae74cc632
begimov <begimov@gmail.com>
2018-10-26 13:48:38 +0300

    basic Stocks component created

---
+++ resources/js/components/Stocks.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header text-white bg-primary">Список валют</div>
+
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead class="bg-dark text-white">
+                                <tr>
+                                    <th scope="col">Название</th>
+                                    <th scope="col">Цена</th>
+                                    <th scope="col">Количество</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>//</td>
+                                    <td>//</td>
+                                    <td>//</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            //
+        }
+    }
+</script>
