ea1f2cba85a1676d67ce13183c06ce3fb6ccdc84
begimov <begimov@gmail.com>
2018-10-26 11:20:25 +0300

    StockRepository implementation instance injected into StockController's constructor, RepositoryServiceProvider registered in app config

--- app/Http/Controllers/Stocks/StockController.php
+++ app/Http/Controllers/Stocks/StockController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Stocks;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\Stocks\StockRepository;
 
 class StockController extends Controller
 {
+    protected $stocks;
+
+    public function __construct(StockRepository $stocks)
+    {
+        $this->stocks = $stocks;
+    }
     public function index()
     {
-        //
+        dd($this->stocks);
     }
 }
--- app/Repositories/Remote/Stocks/RemoteStockRepository.php
+++ app/Repositories/Remote/Stocks/RemoteStockRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\Remote\Stocks;
 
 use App\Repositories\Remote\RemoteRepositoryAbstract;
+use App\Repositories\Contracts\Stocks\StockRepository;
 
 class RemoteStockRepository extends RemoteRepositoryAbstract implements StockRepository
 {
