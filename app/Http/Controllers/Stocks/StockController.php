d795ef19ffe2eaa1d54c92cd16b2894ae9bf327b
begimov <begimov@gmail.com>
2018-10-26 10:55:09 +0300

    StockController created

---
+++ app/Http/Controllers/Stocks/StockController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Stocks;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StockController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
